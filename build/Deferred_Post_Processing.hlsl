b524b686ebcc2bd67d73df89378b11f82540cfff
David Headrick <david.headrick9@gmail.com>
2023-12-30 10:34:53 -0800

    Added Uncharted2 Tone Mapping

--- build/Deferred_Post_Processing.hlsl
+++ build/Deferred_Post_Processing.hlsl
@@ -11,8 +11,18 @@ RWTexture2D<float4> texture_2d_uav_table[] : register(u0, space99);
 ConstantBuffer<SSAO_Texture_Index> output_texture_index : register(b0, ComputeSpace);
 ConstantBuffer<SSAO_Texture_Index> ssao_texture_index   : register(b1, ComputeSpace);
 
+static const float A = 0.15;
+static const float B = 0.50;
+static const float C = 0.10;
+static const float D = 0.20;
+static const float E = 0.02;
+static const float F = 0.30;
+static const float W = 11.2;
 
-
+float3 Uncharted2Tonemap(float3 x)
+{
+   return ((x*(A*x+C*B)+D*E)/(x*(A*x+B)+D*F))-E/F;
+}
 
 // The compute shader
 [numthreads(GROUP_SIZE_X, GROUP_SIZE_Y, 1)]
@@ -25,14 +35,26 @@ void main(uint3 DTid : SV_DispatchThreadID)
     
     // Tone Map and Gamma Correct
     color *= ssao_occlusion;
-    //color *= 5;
+    // color *= 4.0;
+    // color *= 0.5;
 
     // Reinhert
     color = color / (color + float3(1.0, 1.0, 1.0));
 
     // Filmic curve from: http://filmicworlds.com/blog/filmic-tonemapping-operators/
+    // NO GAMMA ADJ REQUIRED, BAKED IN
     // color = max(float3(0.0, 0.0, 0.0), color-0.004);
     // color = (color*(6.2*color+.5))/(color*(6.2*color+1.7)+0.06);
+   
+    // Uncharted2 tonemap from: http://filmicworlds.com/blog/filmic-tonemapping-operators/
+    // Everything looks ok, except for the floor looks too bright??
+    // float ExposureBias = 2.0f;
+    // float3 curr = Uncharted2Tonemap(ExposureBias*color);
+
+    // float3 whiteScale = 1.0f/Uncharted2Tonemap(W);
+    // color = curr*whiteScale;
+
+    // Gamma Correction
     color = pow(color, float3(1.0/2.2, 1.0/2.2, 1.0/2.2));  
 
     // Write to output texture
