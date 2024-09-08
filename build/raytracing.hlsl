3b7e3224137324b1a11b9fd59474557e2917443f
David Headrick <david.headrick9@gmail.com>
2024-09-08 11:10:56 -0700

    Fix Normals sampled from Texture

--- build/raytracing.hlsl
+++ build/raytracing.hlsl
@@ -77,7 +77,9 @@ struct Hit_Info {
     float2 uv;
     float3 p;
     float3 n;
+    float3 wn;
     float3 t;
+    float  t_handedness;
     uint material_id;
     float2 ddx;
     float2 ddy;
@@ -410,7 +412,7 @@ BSDF_Sample BxDF_diffuse_sample_f(float3 wo, float3 albedo, float2 random_u, Hit
     w_Per_Vertex_Tangent = normalize(w_Per_Vertex_Tangent - dot(w_Per_Vertex_Tangent, w_Per_Vertex_Normal) * w_Per_Vertex_Normal );
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * float3(-1.0, -1.0, -1.0);// * -IN.tangent_handidness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
+    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * -hit_info.t_handedness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
 
     float3x3 TBN = float3x3( normalize(w_Per_Vertex_Tangent), normalize(w_Per_Vertex_Bitangent), normalize(w_Per_Vertex_Normal) );
     // TBN = transpose( TBN );
@@ -530,7 +532,7 @@ float3 BxDF_TS_f(float3 wo, float3 wi, float3 albedo, Hit_Info hit_info, float m
     w_Per_Vertex_Tangent = normalize(w_Per_Vertex_Tangent - dot(w_Per_Vertex_Tangent, w_Per_Vertex_Normal) * w_Per_Vertex_Normal );
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * float3(-1.0, -1.0, -1.0);// * -IN.tangent_handidness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
+    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * -hit_info.t_handedness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
 
     float3x3 TBN = float3x3( normalize(w_Per_Vertex_Tangent), normalize(w_Per_Vertex_Bitangent), normalize(w_Per_Vertex_Normal) );
     float3x3 inv_TBN = transpose(TBN);
@@ -574,7 +576,7 @@ BSDF_Sample BxDF_TS_sample_f(float3 wo, float3 albedo, float2 random_u, Hit_Info
     w_Per_Vertex_Tangent = normalize(w_Per_Vertex_Tangent - dot(w_Per_Vertex_Tangent, w_Per_Vertex_Normal) * w_Per_Vertex_Normal );
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * float3(-1.0, -1.0, -1.0);// * -IN.tangent_handidness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
+    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * -hit_info.t_handedness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
 
     float3x3 TBN = float3x3( normalize(w_Per_Vertex_Tangent), normalize(w_Per_Vertex_Bitangent), normalize(w_Per_Vertex_Normal) );
     float3x3 inv_TBN = transpose(TBN);
@@ -730,7 +732,7 @@ void MyRaygenShader()
     payload.color = float4(0.0, 0.0, 0.0, 0);
     payload.random_u = random_u;
 
-    static const uint SAMPLE_COUNT = 20;
+    static const uint SAMPLE_COUNT = 25;
     RayDesc ray;
     for(uint i = 0; i < SAMPLE_COUNT; i++){
 
@@ -791,7 +793,9 @@ Hit_Info get_hit_info(float2 barycentrics_2) {
     hit_info.p  = barycentrics.x * vertex1.position + barycentrics.y * vertex2.position + barycentrics.z * vertex3.position;
     hit_info.n  = barycentrics.x * vertex1.normal   + barycentrics.y * vertex2.normal   + barycentrics.z * vertex3.normal;
     hit_info.n  = normalize(hit_info.n);
-    hit_info.t  = barycentrics.x * vertex1.tangent + barycentrics.y * vertex2.tangent + barycentrics.z * vertex3.tangent;
+    float4 tangent  = barycentrics.x * vertex1.tangent + barycentrics.y * vertex2.tangent + barycentrics.z * vertex3.tangent;
+    hit_info.t = tangent.xyz;
+    hit_info.t_handedness = tangent.w;
     hit_info.t  = normalize(hit_info.t);
     hit_info.material_id = g_info.material_id;
 
@@ -910,23 +914,49 @@ void MySimplePathTracer(inout RayPayload payload : SV_RayPayload, in MyAttribute
     Texture2D roughness_metallic_texture = texture_2d_table[NonUniformResourceIndex(texture_array_begin.texture_index + hit_info.material_id * 3 + 2)];
 
     float4 albedo_sample = albedo_texture.SampleGrad(sampler_1, hit_info.uv, hit_info.ddx, hit_info.ddy);
-    float4 normal_sample = normal_texture.SampleGrad(sampler_1, hit_info.uv, hit_info.ddx, hit_info.ddy);
-    // hit_info.n = normal_sample.xyz;
+    float3 normal_sample = normal_texture.SampleGrad(sampler_1, hit_info.uv, hit_info.ddx, hit_info.ddy).xyz;
+    normal_sample = normalize((2*normal_sample) - 1);
     float4 roughness_metallic_sample = roughness_metallic_texture.SampleGrad(sampler_1, hit_info.uv, hit_info.ddx, hit_info.ddy);
-    float metallic = roughness_metallic_sample.r;
-    float roughness = roughness_metallic_sample.g;//pow(roughness_metallic_sample.g, 2);
+    float metallic = roughness_metallic_sample.b;
+    float roughness = roughness_metallic_sample.g;//1 - roughness_metallic_sample.g;//pow(roughness_metallic_sample.g, 2);
+
+    // Create Tangent-Bitangent-Normal matrix to convert Tangent Space normal to world space normal
+    // https://stackoverflow.com/questions/16555669/hlsl-normal-mapping-matrix-multiplication
+    float3 w_Per_Vertex_Normal  = hit_info.n;
+    float3 w_Per_Vertex_Tangent = hit_info.t;
+    
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Gram - Schmidt process
+    // Re Orthoganalizes the tangent vector ( ensures 90* between Normal and Tangent)
+    // Vectors could be slightly off of 90*
+    // Might be more useful in the pixel shader if we built a TBN matrix there, after interpolating tangent and normal through rasterization
+    // 
+    // Scale Normal by cos(theta), then line between scaled normal and tangent is orthoganal to original normal. Subtract tangent to get new tangent
+    w_Per_Vertex_Tangent = normalize(w_Per_Vertex_Tangent - dot(w_Per_Vertex_Tangent, w_Per_Vertex_Normal) * w_Per_Vertex_Normal );
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    float3 w_Per_Vertex_Bitangent = cross(w_Per_Vertex_Normal, w_Per_Vertex_Tangent) * -hit_info.t_handedness;  // Need to multiplay by (negative) tangent handidness to correct for handidness of textures tangent space and DirectX UV space
+
+    float3x3 TBN = float3x3( normalize(w_Per_Vertex_Tangent), normalize(w_Per_Vertex_Bitangent), normalize(w_Per_Vertex_Normal) );
+
+    //hit_info.wn = mul(TBN, normal_sample.xyz);
+    hit_info.wn = mul(normal_sample.xyz, TBN);
+    float3 delta = hit_info.wn - hit_info.n;
+    hit_info.n = hit_info.wn;
+    //hit_info.t += delta;
+    //roughness = roughness * 0.3;
     //roughness = pow(roughness_metallic_sample.g, 2);
     //roughness = 1. - roughness;
 
     float F;
-    float3 f = BxDF_CT_f(wo, env_light_sample.wi, albedo_sample.rgb, hit_info, metallic, roughness)  * abs(dot(env_light_sample.wi, hit_info.n));//);
-    //float3 f = BxDF_diffuse_f(wo, env_light_sample.wi, albedo_sample.rgb) * abs(dot(env_light_sample.wi, hit_info.n));
+    float3 f = BxDF_CT_f(wo, env_light_sample.wi, albedo_sample.rgb, hit_info, metallic, roughness) * abs(dot(env_light_sample.wi, hit_info.wn));//);
+    // float3 f = BxDF_diffuse_f(wo, env_light_sample.wi, albedo_sample.rgb) * abs(dot(env_light_sample.wi, hit_info.wn));
 
     if(!env_light_sample.occluded)
         payload.color.rgb += (f * payload.beta * env_light_sample.L) / (1 * env_light_sample.pdf);// payload.beta * f * env_light_sample.L / ( 1 * env_light_sample.pdf );
 
     // Sample outgoing direction at intersecion to continue path
-    // u.xy = get_rand_float3(payload.random_u).xy;
+    u.xy = get_rand_float3(payload.random_u).xy;
     BSDF_Sample bsdf_sample;
     if(u.x > 0.5){
         u.xy = get_rand_float3(payload.random_u).xy;
@@ -936,7 +966,7 @@ void MySimplePathTracer(inout RayPayload payload : SV_RayPayload, in MyAttribute
         bsdf_sample = BxDF_diffuse_sample_f(wo, albedo_sample.rgb, u, hit_info);
     }
 
-    payload.beta *= bsdf_sample.sampled_light * abs(dot(bsdf_sample.wi, hit_info.n) / bsdf_sample.pdf );//* 0.5);
+    payload.beta *= bsdf_sample.sampled_light * abs(dot(bsdf_sample.wi, hit_info.wn) / bsdf_sample.pdf );//* 0.5);
     // pbrt handles whether the ray was specular or not
 
     // Create and trace new ray
